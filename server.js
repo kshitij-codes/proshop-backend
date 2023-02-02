be964022cfa8aceaef21a5fce27cb3b8225d6e7e
Gaurang Gaur <kshitijkvy@gmail.com>
2023-02-02 20:53:34 +0530

    backend setup

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import productRoutes from './routes/productRoutes.js'
+
+dotenv.config()
+await connectDB()
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+
+app.use('/api/products', productRoutes)
+
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.use((err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    })
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
