069f00625aa815cdafa10bf015c26f44a20497eb
Gaurang Gaur <kshitijkvy@gmail.com>
2023-02-05 20:13:25 +0530

    Added auth service

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,25 +1,10 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
+import { getProductById, getProducts } from '../controllers/productController.js'
 const router = express.Router()
-import Product from '../models/productModel.js'
 
-// Fetch All Products: GET /api/products
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
 
-// Fetch single product: GET /api/products/:id
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    
-    if(product){
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-}))
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 
 export default router
