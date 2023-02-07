0dc7a2c7ab7b9718ffaa60f99135a821404be71d
Gaurang Gaur <kshitijkvy@gmail.com>
2023-02-07 20:19:29 +0530

    added admin functionality

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -31,6 +31,35 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 })
 
+// Update Order to Paid: GET /api/orders/:id/pay: Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if(order) {
+    order.isPaid = true
+    order.paidAt = Date.now()
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address
+    }
+
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+// Get logged in user orders: GET /api/orders/myorders: Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({user: req.user._id})
+res.json(orders)
+})
+
 export {
-    addOrderItems, getOrderById
+    addOrderItems, getOrderById, updateOrderToPaid, getMyOrders
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -92,7 +92,60 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
+// Get all users: GET /api/users: PRIVATE/ADMIN
+const getUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({})
+    res.json(users) 
+})
+
+// Delete user: DELETE /api/users/:id: PRIVATE/ADMIN
+const deleteUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id)
+    if(user){
+        await user.remove()
+        res.json({message: 'User removed'})
+    } else {
+        res.status(404)
+        throw new Error('user not found')
+    }
+})
+
+// Get user by ID: GET /api/users/:id: PRIVATE/ADMIN
+const getUserById = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id).select('-password')
+    
+    if(user){
+        res.json(user) 
+    } else {
+        res.status(404)
+        throw new Error('user not found')
+    }
+})
+
+// Update user: PUT /api/users/:id: PRIVATE/ADMIN
+const updateUser = asyncHandler(async (req, res) => {
+      
+    const user = await User.findById(req.params.id)
+
+    if(user){
+        user.name = req.body.name || user.name,
+        user.email = req.body.email || user.email
+        user.isAdmin = req.body.isAdmin
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin
+        })
+    }else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
 
 export {
-    authUser, getUserProfile, registerUser, updateUserProfile
+    authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser, updateUser, getUserById
 }
